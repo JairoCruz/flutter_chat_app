6d15a7b772248a244eddcc5d8e466550ee283455
Jairo Cruz <azaveliz@example.com>
2023-06-19 13:56:32 -0600

    add behavior to the TextFormField

--- lib/presentacion/widgets/shared/message_field_box.dart
+++ lib/presentacion/widgets/shared/message_field_box.dart
@@ -11,6 +11,10 @@ class MessageFieldBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+
     final outlineInputBorder = UnderlineInputBorder(
       borderSide: const BorderSide(color: Colors.transparent),
       borderRadius: BorderRadius.circular(40)
@@ -18,25 +22,32 @@ class MessageFieldBox extends StatelessWidget {
 
 
     final inputDecoration = InputDecoration(
+        hintText: 'End your message with a "??"',
         enabledBorder: outlineInputBorder,
         focusedBorder: outlineInputBorder,
         filled: true,
         suffixIcon: IconButton(
           icon: const Icon(Icons.send_outlined),
           onPressed: () {
-            print('Valor de caja de texto');
+            final textValue = textController.value.text;
+            print('button: $textValue');
+            textController.clear();
           },),
         
       );
 
     return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
         print('submit value $value');
-      },
-      onChanged: (value) {
-        print('changed: $value');
-      },
+        textController.clear();
+        focusNode.requestFocus();
+      }
     );
   }
 }
