211beafe75c2e79f7c20d9bb1a8f47996649e6bc
Jairo Cruz <azaveliz@example.com>
2023-07-10 10:12:17 -0600

    add domain

--- lib/presentacion/widgets/shared/message_field_box.dart
+++ lib/presentacion/widgets/shared/message_field_box.dart
@@ -5,9 +5,6 @@ import 'package:flutter/material.dart';
 class MessageFieldBox extends StatelessWidget {
   const MessageFieldBox({super.key});
 
-
-  
-
   @override
   Widget build(BuildContext context) {
 
